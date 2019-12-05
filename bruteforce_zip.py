05a9933632ccaa5956940f582e0787ce4cabf706
Angular Orbit <AngularOrbit@gmail.com>
2019-12-05 21:23:14 +1100

    Added bruteforce script

---
+++ bruteforce_zip.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import zipfile
+import argparse
+
+parser = argparse.ArgumentParser(description='Bruteforce the password of a zip file.')
+parser.add_argument('zipfile',type=str)
+parser.add_argument('wordlist',type=str)
+
+args = parser.parse_args()
+zip_file_name = args.zipfile
+wordlist_name = args.wordlist
+
+with zipfile.ZipFile(zip_file_name,'r') as zip_file:
+    with open(wordlist_name,'r') as wordlist:
+        for line in wordlist:
+            try :
+                password_bytes = line.rstrip('\n').encode('utf-8')
+                zip_file.extractall(pwd=password_bytes)
+                print('success')
+                try :
+                    zip_file.extractall()
+                    print('zip is unencrypted');
+                except:
+                    print( 'password is {}'.format(line.rstrip('\n')) )
+                break
+            except:
+                pass
+        else:
+            print('fail, no password matched')
